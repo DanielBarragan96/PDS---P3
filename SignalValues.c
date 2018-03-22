7fa9ed8b0a52f95b501d6359fd5d2ff50b0d6cd0
DanielBarragan96 <ie702554@iteso.mx>
2018-03-21 18:57:10 -0600

    Electric guitar sounds

--- SignalValues.c
+++ SignalValues.c
@@ -1,17 +1,18 @@
 
 #include "SignalValues.h"
+#include "DataTypeDefinitions.h"
 
 static uint16_t g_counter_sin = 0;
 static uint16_t g_counter_song = 0;
+static uint16_t g_counter_evolve = 1.00f;
 static volatile bool change_note = true;
 
 static uint32_t Song[] =
 {
-    DO,DO,SOL,SOL,LA,LA,SOL,SILENCE,FA,FA,MI,MI,RE,RE,DO,
+    DO,DO,SOL,SOL,LA,LA,END,    SOL,SILENCE,FA,FA,MI,MI,RE,RE,DO,
     SOL,SOL,FA,FA,MI,MI,RE,SILENCE,SOL,SOL,FA,FA,MI,MI,DO,
     DO,DO,SOL,SOL,LA,LA,SOL,SILENCE,FA,FA,MI,MI,RE,RE,DO,END
 };
-
 
 uint32_t nextDACValue()
 {
@@ -27,7 +28,8 @@ uint32_t nextDACValue()
         {
             g_counter_sin=0;
         }
-        return SinusOutputData[g_counter_sin];
+
+        return (uint32_t) (SinusOutputData[g_counter_sin]*envolvente[g_counter_evolve]/12288);
     }
 }
 
@@ -90,5 +92,13 @@ uint32_t nextSongNote()
         }
         change_note = true;
         g_counter_sin=0;
+        g_counter_evolve=0;
         return ldval;
+}
+
+void increaseEnvolv()
+{
+    g_counter_evolve++;
+    if(FUNCTION_LENGTH <= g_counter_evolve)
+        g_counter_evolve=0;
 }
